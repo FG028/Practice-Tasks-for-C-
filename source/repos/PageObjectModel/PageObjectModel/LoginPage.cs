9935d06ecc2373f3fd373742cdcf2a962fcd93ec
Gabor Fazekas <gabor.fazekas@nixs.com>
2024-02-13 17:35:43 +0100

    PageObjectModel Practice Task

---
+++ source/repos/PageObjectModel/PageObjectModel/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+
+namespace PageObjectModel
+{
+        public class LoginPage
+        {
+            private IWebDriver driver;
+
+            public LoginPage(IWebDriver _driver)
+            {
+                driver = _driver;
+            }
+            public string GetLostPasswordText()
+            {
+                var lostPasswordLink = driver.FindElement(By.LinkText("Lost your password?"));
+                return lostPasswordLink.Text;
+            }
+            public string GetRememberMeText()
+            {
+                var rememberMeCheckBox = driver.FindElement(By.Id("rememberme"));
+                return rememberMeCheckBox.Text;
+            }
+            public string GetRegisterButtonText()
+            {
+                var registerButton = driver.FindElement(By.XPath("/ html / body / div[1] / div[2] / div / div / div / div / div[1] / div / div[2] / form / p[3] / input[3]"));
+                return registerButton.Text;
+            }
+            public void Login(string username, string password)
+            {
+                var usernameField = driver.FindElement(By.Id("username"));
+                var passworField = driver.FindElement(By.Id("password"));
+                var loginButton = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/form/p[3]/input[3]"));
+
+                usernameField.SendKeys(username);
+                passworField.SendKeys(password);
+                loginButton.Click();
+            }
+        }
+}
---
+++ source/repos/PageObjectModel/PageObjectModel/Page_Factory.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace PageObjectModel;
+    internal class Page_Factory : LoginPage
+    {
+        [FindsBy(Id = "lostPassword")]
+        private IWebElement lostPasswordLink { get; set; }
+
+        [FindsBy(Id = "rememberMe")]
+        private IWebElement rememberMeCheckBox { get; set; }
+
+        [FindsBy(XPath = "//button[contains(@text(), 'Register')]")]
+        private IWebElement registerButton { get; set; }
+
+        [FindsBy(Id = "username")]
+        private IWebElement usernameField { get; set; }
+
+        [FindsBy(Id = "password")]
+        private IWebElement passwordField { get; set; }
+
+        [FindsBy(Id = "login")]
+        private IWebElement loginButton { get; set; }
+
+        public Page_Factory(IWebDriver driver) : base(driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        public string GetLostPasswordText()
+        {
+            return lostPasswordLink.Text;
+        }
+        public string GetRememberMeText()
+        {
+            return rememberMeCheckBox.Text;
+        }
+        public string GetRegisterButtonText()
+        {
+            return registerButton.Text;
+        }
+        public void Login(string userName, string password)
+        {
+            usernameField.SendKeys(userName);
+            passwordField.SendKeys(password);
+            loginButton.Click();
+        }
+    }
